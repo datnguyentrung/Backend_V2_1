d65dce6aa9081a58cbcad99646033aed8c7f0d54
Nguyen Trung Dat <datnguyentrung.ptit@gmail.com>
2026-07-07 00:28:57 +0700

    update ClassSessionService

--- src/main/java/com/dat/backend_v2_1/service/Operation/ClassSessionService.java
+++ src/main/java/com/dat/backend_v2_1/service/Operation/ClassSessionService.java
@@ -70,7 +70,7 @@ public class ClassSessionService {
     @Value("${ATTENDANCE_CLOSE_AFTER_END_MINUTES:30}")
     private int attendanceCloseAfterEndMinutes;
 
-    @Scheduled(cron = "0 03 00 * * *")
+    @Scheduled(cron = "0 0 03 * * *")
     @Transactional(rollbackFor = Exception.class)
     public void generateClassSessions() {
         LocalDate today = LocalDate.now();
